0c4aa9ea82334196acd3f036ee8b2c15e5f67382
Lester Wolfheart <lester.wolfheart@gmail.com>
2012-11-04 16:31:07 +0100

    After homework for Task C

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,11 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '#columns #side a', :text => 'Home'
+    assert_select '#columns #side a', :text => 'Questions'
+    assert_select '#columns #side a', :text => 'News'
+    assert_select '#columns #side a', :text => 'Contact'
+    assert_select 'div#time'
   end
 
   test "should get new" do
